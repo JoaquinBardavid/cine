945ebacaa2a679a43250f0177c74a19b997d0c3d
JezkelSaurioRex <joabardavid@gmail.com>
2022-06-26 14:05:08 -0300

    v1.0

--- comercio/frontend/src/componentes/reservas/NuevaReserva.js
+++ comercio/frontend/src/componentes/reservas/NuevaReserva.js
@@ -1,25 +1,21 @@
 import { useEffect, useState } from "react";
-import { getReservasPorPeliID, nuevaReserva } from "../../servicios/ReservaServicio";
 import { Button, MenuItem, Select } from "@mui/material";
+import { nuevaReserva } from "../../servicios/ReservaServicio";
+import { useNavigate } from "react-router-dom";
 
 export default function NuevaReserva(props) {
 
-  const { peliId, titulo } = props;
-  const [APIData, setAPIData] = useState();
+  const { peliId, titulo, reservas } = props;
   const [cantEntradas, setCantEntradas] = useState(0);
-  const [asientos, setAsientos] = useState(0);
+  const [asientos, setAsientos] = useState(40);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    (async () => {
-      const data = await getReservasPorPeliID(peliId)
-      setAPIData(data)
-    })()
-    if (APIData != null) {
-      APIData.forEach(reserva => {
-        setAsientos(asientos + reserva.cantAsientos)
-      });
-    }
-    setAsientos(40 - asientos);
+    let aux = 0;
+    reservas.forEach(r => {
+      aux = aux + r.cantAsientos;
+    });
+    setAsientos(40 - aux)
   }, []);
 
   const validar = async () => {
@@ -31,6 +27,7 @@ export default function NuevaReserva(props) {
         if (cantEntradas <= asientos) {
           await nuevaReserva(cantEntradas, peliId);
           alert("Reserva Completada!")
+          navigate(-1);
         } else {
           alert(`quedan solo ${asientos}`)
         }
@@ -40,22 +37,20 @@ export default function NuevaReserva(props) {
 
   return (
     <div>
-      {APIData != null && APIData != [] ? <h3>QUEDAN {asientos} LIEBRES PARA: {titulo}</h3> : <span>QUEDAN 40 ASIENTOS DISPONIBLES PARA "{titulo}"</span>}
-      <div>
-        <Select displayEmpty onChange={(e) => setCantEntradas(e.target.value)}>
-          <MenuItem disabled>
-            <em>Elija una cantidad de entradas</em>
-          </MenuItem>
-          <MenuItem value={1} > 1 ENTRADA </MenuItem>
-          <MenuItem value={2}> 2 ENTRADAS </MenuItem>
-          <MenuItem value={3}> 3 ENTRADAS </MenuItem>
-          <MenuItem value={4}> 4 ENTRADAS </MenuItem>
-        </Select>
-        <br />
-        <div>Cantidad de entradas seleccionadas: {cantEntradas}</div>
-        <Button onClick={()=>validar()}>Confirmar Reserva</Button>
-        <Button href="/reservas">Volver</Button>
-      </div>
+      <h3>QUEDAN {asientos} ENTRADAS PARA: {titulo}</h3>
+      <Select displayEmpty onChange={(e) => setCantEntradas(e.target.value)}>
+        <MenuItem disabled>
+          <em>Elija una cantidad de entradas</em>
+        </MenuItem>
+        <MenuItem value={1} > 1 ENTRADA </MenuItem>
+        <MenuItem value={2}> 2 ENTRADAS </MenuItem>
+        <MenuItem value={3}> 3 ENTRADAS </MenuItem>
+        <MenuItem value={4}> 4 ENTRADAS </MenuItem>
+      </Select>
+      <br />
+      <div>Cantidad de entradas seleccionadas: {cantEntradas}</div>
+      <Button onClick={() => validar()}>Confirmar Reserva</Button>
+      <Button href="/reservas">Volver</Button>
     </div>
   );
 }
--- comercio/frontend/src/paginas/reservas/ReservaPelicula.js
+++ comercio/frontend/src/paginas/reservas/ReservaPelicula.js
@@ -2,23 +2,28 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getPeliculasPorId } from "../../servicios/PeliculaServicio";
+import { getReservasPorPeliID } from "../../servicios/ReservaServicio";
 import NuevaReserva from "../../componentes/reservas/NuevaReserva";
 
 export default function ReservaPeliculas() {
     const { id } = useParams();
 
-    const [APIData, setAPIData] = useState()
+    const [APIPelicula, setAPIPelicula] = useState()
+    const [APIReservas, setAPIReservas] = useState()
 
     useEffect(() => {
         (async () => {
-            const data = await getPeliculasPorId(id)
-            setAPIData(data)
+            const dataPel = await getPeliculasPorId(id)
+            setAPIPelicula(dataPel)
+            const dataRes = await getReservasPorPeliID(id)
+            setAPIReservas(dataRes)
         })()
+
     }, []);
 
     return (
         <>
-            {APIData? <NuevaReserva peliId={APIData.id} titulo={APIData.titulo} /> : <span>Espere...</span>}
+            {APIReservas? <NuevaReserva peliId={id} titulo={APIPelicula.titulo} reservas={APIReservas} /> : <span>Espere...</span>}
         </>
     );
 }
